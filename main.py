6821567886afeb40ee7f3f0cc21570927f3377c8
Research-Mini <jm98675@gmail.com>
2023-02-04 17:26:26 +0900

    conf: main.py menu

---
+++ main.py
@@ -0,0 +1,20 @@
+print("메뉴를 선택하세요> 1: 덧셈, 2:뺄셈, 3:곱셈, 4:나눗셈, 5:종료")
+
+while True:
+menunum=0
+menunum=int(input("메뉴 번호 입력: "))
+
+A = int(input("input 1: "))
+B = int(input("input 2: "))
+
+if menunum==1:
+
+elif menunum==2:
+
+elif menunum==3:
+
+elif menunum==4:
+
+elif menunum==5:
+    break
+
